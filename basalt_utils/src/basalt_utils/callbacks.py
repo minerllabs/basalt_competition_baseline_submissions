ef72e587acf29f69e7041a82d9d5a177a02510dd
Cody Wild <codywild@berkeley.edu>
2021-06-25 13:30:13 -0700

    Add utility to save policy every K batches

--- basalt_utils/src/basalt_utils/callbacks.py
+++ basalt_utils/src/basalt_utils/callbacks.py
@@ -8,6 +8,38 @@ from minerl.herobraine.wrappers.video_recording_wrapper import VideoRecordingWra
 from stable_baselines3.common.vec_env import DummyVecEnv
 
 
+class MultiCallback:
+    """Callback that allows multiple callbacks to be passed into `on_epoch_end`"""
+    def __init__(self, callbacks):
+        self.callbacks = callbacks
+
+    def __call__(self, **kwargs):
+        for callback in self.callbacks:
+            callback(**kwargs)
+
+
+class BCModelSaver:
+    """Callback that saves BC policy every N batches."""
+    def __init__(self, policy, save_dir, save_interval_batches):
+        self.policy = policy
+        self.save_dir = save_dir
+        self.last_save_batches = 0
+        self.save_interval_batches = save_interval_batches
+        self.batch_count = 0
+
+    def __call__(self, **kwargs):
+        """It is assumed that this is called on batch end."""
+        self.batch_count += 1
+        if self.batch_count >= self.last_save_batches + self.save_interval_batches:
+            os.makedirs(self.save_dir, exist_ok=True)
+            save_fn = f'policy_{self.batch_count:08d}_batches.pt'
+            save_path = os.path.join(self.save_dir, save_fn)
+            th.save(self.policy, save_path)
+            print()
+            print(f"Saved policy to {save_path}!")
+            self.last_save_batches = self.batch_count
+
+
 class BatchEndIntermediateRolloutEvaluator:
     """Callback that saves BC policy every K batches."""
     def __init__(self, policy, env, save_dir, evaluate_interval_batches,
