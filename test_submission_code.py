3d31efb9d45daad6b0ac97d28ad419b7e8af6271
Cody Wild <codywild@berkeley.edu>
2021-06-29 14:45:21 -0700

    Cause model to be on auto-detected device

--- test_submission_code.py
+++ test_submission_code.py
@@ -1,6 +1,7 @@
 import gym
 import torch as th
 from basalt_baselines.bc import bc_baseline, WRAPPERS as bc_wrappers
+from stable_baselines3.common.utils import get_device
 import numpy as np
 
 class EpisodeDone(Exception):
@@ -79,7 +80,7 @@ class MineRLAgent():
 class MineRLBehavioralCloningAgent(MineRLAgent):
     def load_agent(self):
         # TODO not sure how to get us to be able to load the policy from the right agent here
-        self.policy = th.load("train/trained_policy.pt")
+        self.policy = th.load("train/trained_policy.pt", map_location=th.device(get_device('auto')))
         self.policy.eval()
 
     def run_agent_on_episode(self, single_episode_env : Episode):
